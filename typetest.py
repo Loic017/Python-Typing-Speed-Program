beacb85234c4ac659850df840b854e5468e4cb76
C21066192 <l.loic017@gmail.com>
2022-07-05 21:15:30 +0100

    def main()
    - Imported curses.
    - Created main() function.

---
+++ typetest.py
@@ -0,0 +1,11 @@
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Hello world!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+wrapper(main)
+    
